3edbedee0571579047211e9da32d1719af813cf5
ChristianStefaniw <christian.stefaniw@student.tdsb.on.ca>
2020-11-25 14:36:05 -0500

    binary search

---
+++ Searching/binary_search.js
@@ -0,0 +1,23 @@
+function binary_search(arr, x){
+    let start = 0;
+    let end = arr.length-1;
+    let mid = 0;
+
+    while (end >= start){
+        mid = Math.floor((start+end)/2)
+        if (arr[mid] == x){
+            return mid;
+        }
+
+        if (arr[mid] > x){
+            end = mid-1;
+        }
+
+        else if (arr[mid] < x){
+            start = mid+1;
+        }
+
+    }
+}
+
+console.log(binary_search(Array(1,73,78,86,100,101), 86));
